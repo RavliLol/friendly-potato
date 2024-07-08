2210fba342887d2a7bfb5a03a236479b2e2d4f47
RavliLol <88993771+RavliLol@users.noreply.github.com>
2024-07-08 15:38:36 +0000

    Pending changes exported from your codespace

---
+++ commands/Information/help.js
@@ -0,0 +1,41 @@
+const { MessageEmbed, Message } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+module.exports = {
+    name: "help",
+    category: "Information",
+    aliases: ["h", "halp"],
+    cooldown: 4,
+    usage: "help",
+    description: "Shows you Help for Garuda Music",
+    run: async (client, message, args, user, text, prefix) => {
+      try{
+        let string = `:white_check_mark: Type \`${prefix}commands\` for a list of Commands\n\n:question:`
+        let embed = new MessageEmbed()
+        .setTitle("Garuda Music")
+        .setDescription(string)
+        if(message.guild.me.permissionsIn(message.channel).has("EMBED_LINKS")){
+          message.reply(embed)
+        }else{
+          message.reply(string)
+        }
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR | An error occurred`)
+            .setDescription(`\`\`\`${e.stack}\`\`\``)
+        );
+    }
+  }
+}
+/**
+  * @INFO
+  * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template
+  * @INFO
+  * Work for Milrato Development | https://milrato.eu
+  * @INFO
+  * Please mention Him / Milrato Development, when using this Code!
+  * @INFO
+*/
